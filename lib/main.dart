f212a29d29b478f324eb0c9f541d1c244574a104
Flávio <f.augustosdn@gmail.com>
2024-06-21 22:31:39 -0300

    feat: implementado layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,12 +11,52 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Lista de tarefas',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: Container(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Tarefas",
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: Container(
+          child: Stack(
+            children: [
+              Container(
+                height: 140,
+                color: Colors.blueAccent,
+              ),
+              Container(
+                height: 100,
+                color: Colors.white,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Container(
+                      height: 100,
+                      width: 72,
+                      color: Colors.black26,
+                    ),
+                    Text("Aprendendo Flutter"),
+                    ElevatedButton(
+                        onPressed: () {}, child: Icon(Icons.arrow_drop_up))
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(onPressed: () {
+          print("Hello World!");
+        }),
+      ),
     );
   }
 }
