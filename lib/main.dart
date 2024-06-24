21f95900c54dbf754ed147d45e42679c792d2a28
Flávio <f.augustosdn@gmail.com>
2024-06-24 18:00:18 -0300

    feat: implementada funcionalidade para escondes tasks

--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool opacity = true;
+
+  hideAndShowTask() {
+    setState(() {
+      opacity = !opacity;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,34 +43,44 @@ class MyApp extends StatelessWidget {
         ),
         body: Padding(
           padding: EdgeInsets.all(10),
-          child: ListView(
-            children: [
-              Task(
-                taskName: "Aprender flutter",
-                image:
-                    "https://images.unsplash.com/photo-1617040619263-41c5a9ca7521?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                difficulty: 3,
-              ),
-              Task(
-                taskName: "Aprender dart",
-                image:
-                    "https://img.icons8.com/?size=100&id=7AFcZ2zirX6Y&format=png&color=000000",
-                difficulty: 3,
-              ),
-              Task(
-                taskName: "Conseguir a primeira vaga mobile",
-                image:
-                    "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-                difficulty: 5,
-              ),
-              Task(
-                taskName: "Aprender uma das tecnologias nativas",
-                image:
-                    "https://img.icons8.com/?size=100&id=24465&format=png&color=000000",
-                difficulty: 4,
-              ),
-            ],
+          child: AnimatedOpacity(
+            opacity: opacity ? 1 : 0,
+            duration: Duration(milliseconds: 400),
+            child: ListView(
+              children: [
+                Task(
+                  taskName: "Aprender flutter",
+                  image:
+                      "https://images.unsplash.com/photo-1617040619263-41c5a9ca7521?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  difficulty: 3,
+                ),
+                Task(
+                  taskName: "Aprender dart",
+                  image:
+                      "https://img.icons8.com/?size=100&id=7AFcZ2zirX6Y&format=png&color=000000",
+                  difficulty: 3,
+                ),
+                Task(
+                  taskName: "Conseguir a primeira vaga mobile",
+                  image:
+                      "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                  difficulty: 5,
+                ),
+                Task(
+                  taskName: "Aprender uma das tecnologias nativas",
+                  image:
+                      "https://img.icons8.com/?size=100&id=24465&format=png&color=000000",
+                  difficulty: 4,
+                ),
+              ],
+            ),
           ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            hideAndShowTask();
+          },
+          child: Icon(opacity ? Icons.toggle_on : Icons.toggle_off_outlined),
         ),
       ),
     );
