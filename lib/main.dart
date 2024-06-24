93562b532cea9a6cd6fbcef84d0c7e94627f686f
Flávio <f.augustosdn@gmail.com>
2024-06-24 16:36:40 -0300

    feat: implementada dificuldade para tarefa

--- lib/main.dart
+++ lib/main.dart
@@ -34,30 +34,27 @@ class MyApp extends StatelessWidget {
             children: [
               Task(
                 taskName: "Aprender flutter",
+                image:
+                    "https://images.unsplash.com/photo-1617040619263-41c5a9ca7521?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                difficulty: 3,
               ),
               Task(
                 taskName: "Aprender dart",
+                image:
+                    "https://img.icons8.com/?size=100&id=7AFcZ2zirX6Y&format=png&color=000000",
+                difficulty: 3,
               ),
               Task(
                 taskName: "Conseguir a primeira vaga mobile",
+                image:
+                    "https://images.unsplash.com/photo-1432888498266-38ffec3eaf0a?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                difficulty: 5,
               ),
               Task(
                 taskName: "Aprender uma das tecnologias nativas",
-              ),
-              Task(
-                taskName: "Aprender uma das tecnologias nativas",
-              ),
-              Task(
-                taskName: "Aprender uma das tecnologias nativas",
-              ),
-              Task(
-                taskName: "Aprender uma das tecnologias nativas",
-              ),
-              Task(
-                taskName: "Aprender uma das tecnologias nativas",
-              ),
-              Task(
-                taskName: "Aprender uma das tecnologias nativas",
+                image:
+                    "https://img.icons8.com/?size=100&id=24465&format=png&color=000000",
+                difficulty: 4,
               ),
             ],
           ),
@@ -69,9 +66,13 @@ class MyApp extends StatelessWidget {
 
 class Task extends StatefulWidget {
   final String taskName;
+  final String image;
+  final int difficulty;
 
   Task({
     required this.taskName,
+    required this.image,
+    required this.difficulty,
     super.key,
   });
 
@@ -83,14 +84,15 @@ class _TaskState extends State<Task> {
   int nivel = 0;
 
   incrementLevel() {
-    if (nivel >= 15) {
-      return;
-    }
     nivel++;
   }
 
   incrementProgressBar() {
-    return nivel / 10;
+    if (widget.difficulty > 0) {
+      return (nivel / widget.difficulty) / 10;
+    } else {
+      return 1;
+    }
   }
 
   @override
@@ -115,16 +117,65 @@ class _TaskState extends State<Task> {
                       height: 100,
                       width: 72,
                       color: Colors.black26,
-                    ),
-                    Container(
-                      width: 200,
-                      child: Text(
-                        widget.taskName,
-                        style: const TextStyle(
-                          fontSize: 18,
-                          overflow: TextOverflow.ellipsis,
-                        ),
+                      child: Image.network(
+                        widget.image,
+                        fit: BoxFit.cover,
                       ),
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 200,
+                          child: Text(
+                            widget.taskName,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 1)
+                                  ? Colors.black
+                                  : Colors.black45,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 2)
+                                  ? Colors.black
+                                  : Colors.black45,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 3)
+                                  ? Colors.black
+                                  : Colors.black45,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 4)
+                                  ? Colors.black
+                                  : Colors.black45,
+                            ),
+                            Icon(
+                              Icons.star,
+                              size: 15,
+                              color: (widget.difficulty >= 5)
+                                  ? Colors.black
+                                  : Colors.black45,
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
                     Padding(
                       padding: const EdgeInsets.only(right: 10),
