cd42a49b31a094b48d8abf9417b33eb4473a8243
Flávio <f.augustosdn@gmail.com>
2024-06-27 22:39:16 -0300

    feat: implementada validações no formulári

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -12,99 +12,126 @@ class _FormScreenState extends State<FormScreen> {
   TextEditingController taskDifficultyController = TextEditingController();
   TextEditingController taskImageController = TextEditingController();
 
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Nova tarefa"),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Container(
-            height: 850,
-            width: 450,
-            decoration: BoxDecoration(
-              border: Border.all(width: 1, color: Colors.black38),
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.black12,
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Escreva o nome da tarefa",
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
-                    controller: taskNameController,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Escreva a dificuldade em números",
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
-                    controller: taskDifficultyController,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    onChanged: (text) {
-                      setState(() {});
-                    },
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      hintText: "Selecione a imagem desejada",
-                      fillColor: Colors.white70,
-                      filled: true,
-                    ),
-                    controller: taskImageController,
-                  ),
-                ),
-                Container(
-                  margin: EdgeInsets.only(bottom: 8),
-                  height: 100,
-                  width: 72,
-                  decoration: BoxDecoration(
-                    border: Border.all(width: 1, color: Colors.black38),
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.black12,
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.network(
-                      taskImageController.text,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Image.asset(
-                          '../assets/images/notfound.webp',
-                          fit: BoxFit.cover,
-                        );
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Nova tarefa"),
+        ),
+        body: Center(
+          child: SingleChildScrollView(
+            child: Container(
+              height: 850,
+              width: 450,
+              decoration: BoxDecoration(
+                border: Border.all(width: 1, color: Colors.black38),
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.black12,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value != null && value.isEmpty) {
+                          return 'Você esqueceu de preencher o campo';
+                        }
+                        return null;
                       },
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Escreva o nome da tarefa",
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                      controller: taskNameController,
                     ),
                   ),
-                ),
-                ElevatedButton(
-                    onPressed: () {
-                      print(taskNameController.text);
-                      print(int.parse(taskDifficultyController.text));
-                      print(taskImageController);
-                    },
-                    child: Text("Adicionar"))
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) > 5 ||
+                            int.parse(value) < 1) {
+                          return "Insira uma dificuldade entre 1 e 5";
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Escreva a dificuldade em números",
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                      controller: taskDifficultyController,
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      onChanged: (text) {
+                        setState(() {});
+                      },
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Insira uma URL de imagem';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.url,
+                      textAlign: TextAlign.center,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: "Selecione a imagem desejada",
+                        fillColor: Colors.white70,
+                        filled: true,
+                      ),
+                      controller: taskImageController,
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 8),
+                    height: 100,
+                    width: 72,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 1, color: Colors.black38),
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.black12,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Image.network(
+                        taskImageController.text,
+                        fit: BoxFit.cover,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset(
+                            '../assets/images/notfound.webp',
+                            fit: BoxFit.cover,
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          return;
+                        }
+                      },
+                      child: const Text("Adicionar"))
+                ],
+              ),
             ),
           ),
         ),
