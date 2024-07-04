114a8ea061efebece419cbbcc2cf5f7f2eb89556
Flávio <f.augustosdn@gmail.com>
2024-07-03 23:41:11 -0300

    feat: implementado para listar todas as tarefas

--- lib/data/task_dao.dart
+++ lib/data/task_dao.dart
@@ -1,4 +1,6 @@
 import 'package:meu_primeiro_projeto/components/task.dart';
+import 'package:meu_primeiro_projeto/data/database.dart';
+import 'package:sqflite/sqflite.dart';
 
 class TaskDao {
   static const String tableSql = 'CREATE TABLE $_tablename('
@@ -12,7 +14,28 @@ class TaskDao {
   static const String _image = 'image';
 
   save(Task tarefa) async {}
-  Future<List<Task>> findAll() async {}
+  Future<List<Task>> findAll() async {
+    final Database database = await getDataBase();
+    final List<Map<String, dynamic>> result = await database.query(_tablename);
+
+    return toList(result);
+  }
+
+  List<Task> toList(List<Map<String, dynamic>> taskList) {
+    final List<Task> tasks = [];
+    for (Map<String, dynamic> data in taskList) {
+      final Task task = Task(
+        taskName: data[_name],
+        image: data[_image],
+        difficulty: data[_difficulty],
+      );
+
+      tasks.add(task);
+    }
+
+    return tasks;
+  }
+
   Future<List<Task>> find(String taskName) async {}
   delete(String taskName) async {}
 }
